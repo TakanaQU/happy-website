c8107f21d1973353c7a4d9f3a84095bccc3ba2e7
TakanaQU <sthsth.u.negi@gmail.com>
2023-04-25 15:29:12 +0900

    set backgroundimage 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import clsx from 'clsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <div className="App">
+        
+          
+          <header className="App-header">       
+            <div style={{ backgroundImage: "url(/vortex.png"}}>
+            <img src={'/smile.svg'} className="SmilingImage" alt="smiling" />
+            <img src={'/laying.svg'} className="SmilingImage" alt="smiling" />
+            <p>
+              死は救済なのか？　自分で確かめるしかないのか？
+            </p>
+            <p>
+              確かめるにはこちら↓
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </div>
+        </header>
     </div>
   );
 }
