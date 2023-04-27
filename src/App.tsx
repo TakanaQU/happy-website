fc2fabffa689884d13dd98e432ace65f036500ef
TakanaQU <sthsth.u.negi@gmail.com>
2023-04-27 15:05:25 +0900

    add positive and negativemessage and change link

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ function App() {
           
           <header className="App-header">    
           言いたいことも言えないこんな世の中<br />
-          うんざりしていませんか？   
+          うんざりしていませんか？
             <div style={{ backgroundImage: "url(/vortex.png)"}}>
-            <img src={'/smile.svg'} className="SmilingImage" alt="smiling" />
-            <img src={'/laying.svg'} className="SmilingImage" alt="smiling" />
+            <img src={'/smile.svg'} className="SmilingImage" alt="smiling" /><br />
+            <img src={'/laying.svg'} className="SmilingImage" alt="laying" />
             <p>
               死は救済なのか？　自分で確かめるしかないのか？
             </p>
@@ -21,13 +21,26 @@ function App() {
               確かめるにはこちら↓
             </p>
             <a
-              className="App-link"
-              href="https://reactjs.org"
+              className="Abehiroshi-link"
+              href="http://abehiroshi.la.coocan.jp/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learn React
+              学校では教えてくれない世界の真理（ひ♡み♡つ）
             </a>
+            <br />あ〜5000兆円欲しい<br />
+            <img src={'/smile.svg'} className="SmilingImage" alt="smiling" /><br />
+            人生にログボはないんですか？
+            <img src={'/smile.svg'} className="SmilingImage" alt="smiling" /><br />
+            石油王道案内したら100万くらいくれないかな〜
+            <img src={'/smile.svg'} className="SmilingImage" alt="smiling" /><br />
+            物足りない愛情
+            <img src={'/smile.svg'} className="SmilingImage" alt="smiling" /><br />
+
+            裏
+            反転ボタン
+            全部全部全部壊したい
+
           </div>
         </header>
     </div>
