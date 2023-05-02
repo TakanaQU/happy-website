2053554806f46633f792dd531bbf119579275799
TakanaQU <sthsth.u.negi@gmail.com>
2023-05-02 19:49:56 +0900

    上の余白がフォントサイズ依存なことまでは分かった

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,11 +4,13 @@ function Header() {
     const headerStyle = {
         backgroundColor: 'black',
         color: 'white',
+        margin: 0,
+        height: '3em',
     }
 
     return (
         <header style={headerStyle}>
-            <h1>言いたいことも言えないこんな世の中<br />
+            <h1 style={{fontSize: '30px', lineHeight: '30px'}}>言いたいことも言えないこんな世の中
             うんざりしていませんか？</h1>
         </header>
     )
