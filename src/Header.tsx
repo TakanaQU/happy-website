a5c361f7026521b8751b5287c130f4d64a59d364
TakanaQU <sthsth.u.negi@gmail.com>
2023-05-06 22:55:22 +0900

    大回転ボタンの設置のみ

--- src/Header.tsx
+++ src/Header.tsx
@@ -10,13 +10,33 @@ function Header() {
         top: 0,
     }
 
+    const buttonStyle = {
+        backgroundColor: 'white',
+        color: 'black',
+        border: '2px solid black',
+        padding: '10px',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        borderRadius: '20px',
+        cursor: 'pointer',
+        transition: 'all 0.2s ease-in-out',
+    }
+    
+
+    const [isRotated, setIsRotated] = useState(false);
+
+    const rotationButtonClick = () => {
+        setIsRotated(!isRotated);
+    };
     
 
     return (
         <header style={headerStyle}>
             <h1 style={{fontSize: '50px', lineHeight: '1'}}>言いたいことも言えないこんな世の中
             うんざりしていませんか？</h1>
-            <button></button>
+            <button style={buttonStyle} className={isRotated ? 'rotated': ''} onClick={rotationButtonClick}>
+                大回転
+            </button>
         </header>
     )
 }
